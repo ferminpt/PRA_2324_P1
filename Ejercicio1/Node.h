1e02802053f73932df27e37d4d5aa825a9d0f058
Fermin Perez Torres <fpertor@upv.edu.es>
2024-10-11 11:12:30 +0200

    Añadida implementacion de la clase Node

---
+++ Ejercicio1/Node.h
@@ -0,0 +1,21 @@
+#include <ostream>
+
+template < typename T>
+
+class Node{
+	public:
+T data;
+Node<T>* next;
+
+Node(T data, Node<T>* next=nullptr){
+	data= data;
+	next=next;
+
+}
+
+ // Sobrecarga del operador << para imprimir el nodo
+    friend std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
+        out << node.data;  // Solo se imprime el atributo data
+        return out;
+    }
+};
